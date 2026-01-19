57e9471d7d5b082d6ff18a80ecaf40af655294ca
Emre <mrguven35@gmail.com>
2026-01-19 11:19:52 +0300

    Fix React import duplicate

--- src/pages/Receipts.jsx
+++ src/pages/Receipts.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
 
 
 function Receipts() {
